97dfa5ca2715f54cec1b37cd3f7e055711fcf2b7
skellawu2025 <skellawu@hotmail.com>
2025-03-07 17:39:03 +0800

    Step 7: update listings template

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . models import Listing
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def listings(request):
     #listings = Listing.objects.all()
+    #for listing in listings:
+     #   print(listing)
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
     paginator = Paginator(listings, 3)
     page = request.GET.get('page')
@@ -12,7 +14,9 @@ def listings(request):
     context = {'listings' : paged_listings}
     return render(request, 'listings/listings.html', context)
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {'listing' : listing}
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
     return render(request, 'listings/search.html')
