5101fd631a58a80ccc98f7ebe3b17c0eaa795cd2
skellawu2025 <skellawu@hotmail.com>
2025-03-19 11:51:57 +0800

    step 15 completed

--- bcre/settings.py
+++ bcre/settings.py
@@ -154,3 +154,9 @@ MESSAGE_TAGS = {
     messages.ERROR : 'danger',
     messages.SUCCESS : 'success',
 }
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'skellawu@hotmail.com'
+EMAIL_HOST_PASSWORD = 'Skella10'
+EMAIL_USE_TLS = True
--- contacts/views.py
+++ contacts/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .models import Contact
-
+from django.core.mail import send_mail
 # Create your views here.
 def contact(request):
     if request.method == 'POST':
@@ -12,13 +12,29 @@ def contact(request):
         phone = request.POST['phone']
         message = request.POST['message']
         user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
         if request.user.is_authenticated:
             # user_id = request.user.id
             has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
             if has_contacted:
                 messages.error(request, 'You have already made an inquiry for this listing')
+                return redirect('/listings/'+listing_id)
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
         contact.save()
+        send_mail(
+            'Property Listing Inquiry',
+            'There has been an inquiry for '+listing+'. Sign into the admin panel for more info',
+            'skellawu@hotmail.com',#from email
+            'realtor_email', #to email
+            fail_silently=False
+        )
+
         messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
         return redirect('/listings/'+listing_id)
+
+def delete_contact(request, contact_id):
+    contact=get_object_or_404(Contact, pk=contact_id)
+    contact.delete()
+    return redirect('dashboard')
+    
         
